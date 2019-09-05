f9ec52d2373de31f42df6eb218084c39bed9e16d
Andrey Helldar <helldar@ai-rus.com>
2019-09-05 19:24:21 +0300

    Added condition for loading migrations

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Helldar\SpammersServer;
 
+use function config;
+
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     protected $defer = false;
@@ -11,6 +13,10 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
         $this->publishes([
             __DIR__ . '/config/settings.php' => \config_path('spammers_server.php'),
         ], 'config');
+
+        if (config('spammers_server.type') !== 'remote') {
+            $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+        }
     }
 
     public function register()
